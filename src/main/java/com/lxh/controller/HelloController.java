07bb60adc057ecb50efeb7f6b61f8fce6fd7448e
liuxh2_tydic <liuxiaohe@tydic.com>
2019-07-02 16:40:01 +0800

    【需求】:修改注解方式

--- src/main/java/com/lxh/controller/HelloController.java
+++ src/main/java/com/lxh/controller/HelloController.java
@@ -1,8 +1,7 @@
 package com.lxh.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author 刘晓禾
@@ -11,11 +10,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @description
  */
 
-@Controller
+@RestController
 public class HelloController {
 
 
-    @ResponseBody
     @RequestMapping("/hello")
     public String hello(){
         return "hello";
